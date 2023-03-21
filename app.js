b76afc82cceba3d4ce29ee2367657e1c4178a7bf
minha sohn <codeminhasohn@gmail.com>
2023-03-21 23:56:22 +0900

    [CHORE] 자주 쓰는 middleware 공부 완료

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
-'user strict';
+'use strict';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import dotenv from 'dotenv';
+
 import indexRouter from './routes/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
 const router = express.Router();
+// .env 파일을 읽어서 process.env로 만든다.
+dotenv.config();
 
-//@ router
-// app.use(cookieParser());
+// path 경로: ES6는 따로 설정
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// router
 app.use('/', indexRouter);
-app.use(logErrors);
 
-app.set('port', process.env.PORT || 3000); //@ 설정 포트가 연결이 안되면 3000번으로 연결
+// record requests and reponses to console
+app.use(morgan('dev'));
+
+// provide static files
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// body-parser (create req.body): read request body to process JSON and formdata
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// create req.cookies: compare a signed cookie from browser to prove if the signature is provided from its server
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// manage express-session
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: 'session-cookie',
+  })
+);
+
+// manage port#: connect to 3000 if assigned port isn't operated
+app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), (req, res) => {
   console.log('Server is connecting to port ' + app.get('port'));
 });
-
-function logErrors(err, req, res, next) {
-  console.error(err.stack);
-  next(err);
-}
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,38 @@
 'user strict';
 import express from 'express';
+import userRouter from './user.js';
 const router = express.Router();
 
-import userRouter from './user.js';
-
+//@ router
 router.use('/user', userRouter);
 
-router.get('/', (req, res) => {
-  res.send('hello, hhexpress');
-  // res.sendFile(path.join(__dirname, './views/index.ejs'));
+// router.use((req, res, next) => {
+//   console.log('모든 요청에 다 실행됩니다. ');
+//   next();
+// });
+
+router.get('/', (req, res, next) => {
+  try {
+    console.log('GET / 요쳥에서만 실행됩니다. ');
+    res.send('Welcome to study-node');
+    next();
+  } catch (error) {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+  }
+});
+
+router.post('/', (req, res, next) => {
+  try {
+    res.send('POST / Welcome to study-node');
+    next();
+  } catch (error) {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+  }
+});
+
+router.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
 });
 
 export default router;
